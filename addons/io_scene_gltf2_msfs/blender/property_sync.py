d3fb8cf499a9705a3b9554467f29b920bd22c48f
pepperoni505 <pepperoni505@gmail.com>
2022-04-10 20:06:39 -0400

    wip: sync blender properties with custom properties
    TODO: figure out how to define which properties to sync to

---
+++ addons/io_scene_gltf2_msfs/blender/property_sync.py
@@ -0,0 +1,27 @@
+# glTF-Blender-IO-MSFS
+# Copyright (C) 2021-2022 The glTF-Blender-IO-MSFS authors
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import bpy
+
+
+@bpy.app.handlers.persistent
+def depsgraph_update(scene, depsgraph):
+    for update in depsgraph.updates:
+        pass
+
+
+def register():
+    bpy.app.handlers.depsgraph_update_post.append(depsgraph_update)
